dd6ec7b5d3a8fba8f3192462641e79485b0f31da
LHabc <lhabc.code@outlook.com>
2023-02-02 21:17:19 +0800

    添加反应速度测试组件

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <ReactionTest>
+    </ReactionTest>
+</template>
+
+<script>
+import ReactionTest from "@/components/ReactionTest.vue";
+
+export default {
+    components: {
+        ReactionTest
+    }
+}
+</script>
+
+<style>
+* {
+    padding: 0;
+    margin: 0;
+}
+
+html, body, #app {
+    height: 100%;
+    width: 100%;
+}
+</style>
