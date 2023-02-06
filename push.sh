7e32b8760a75672185b7e8035e710fbffe6685d4
LHabc <lhabc.code@outlook.com>
2023-02-06 21:04:37 +0800

    修改打包时路径错误的问题

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-./build.sh
-zip -r ./build/dist.zip ./build/dist
-scp ./build/dist.zip ubuntu@101.42.33.170:/home/ubuntu/ #使用时修改为自己的服务器地址
+cd ./build
+zip -r ./dist.zip ./dist
+scp ./dist.zip ubuntu@101.42.33.170:/home/ubuntu/ #使用时修改为自己的服务器地址
